59a172f11b1edecd0bc8f869cb373f48ca813084
Fuad <163154164+Ibnmazid@users.noreply.github.com>
2024-12-12 17:55:34 +0600

    Delete .venv directory

--- .venv/bmi_converter.py
+++
@@ -1,7 +0,0 @@
-print("BMI CALCULATOR >>>>")
-weight=float(input("Enter your weight(kg):"))
-height=float(input("Enter yout hight(m):"))
-
-bmi=weight/(height**2)
-
-print(f"Your BMI >>> {bmi}")
--- .venv/practicennn.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    for (int i = 1; i <= 10; i++) {
-        printf("%d\n", i);
-    }
-    // Pauses the terminal (Windows only)
-    return 0;
-}
